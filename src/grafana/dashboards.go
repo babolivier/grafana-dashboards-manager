05ac42989e9d3e850b4cf288064481ceb5c063fb
Brendan Abolivier <contact@brendanabolivier.com>
2018-01-05 17:52:00 +0100

    Send overwrite: true in creation/update requests because else the Grafana API does weird stuff

--- src/grafana/dashboards.go
+++ src/grafana/dashboards.go
@@ -80,7 +80,7 @@ func (c *Client) GetDashboard(URI string) (db *Dashboard, err error) {
 func (c *Client) CreateOrUpdateDashboard(slug string, contentJSON []byte) (err error) {
 	reqBody := dbUpdateRequest{
 		Dashboard: rawJSON(contentJSON),
-		Overwrite: false,
+		Overwrite: true,
 	}
 
 	reqBodyJSON, err := json.Marshal(reqBody)
