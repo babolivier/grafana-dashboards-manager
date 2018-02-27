5203f39154ba534705cba240f2be83b0adc6531e
Brendan Abolivier <contact@brendanabolivier.com>
2018-02-27 14:19:50 +0100

    Comment main functions

--- src/puller/main.go
+++ src/puller/main.go
@@ -11,17 +11,23 @@ import (
 )
 
 func main() {
+	// Define this flag in the main function because else it would cause a
+	// conflict with the one in the pusher.
 	configFile := flag.String("config", "config.yaml", "Path to the configuration file")
 	flag.Parse()
 
+	// Load the logger's configuration.
 	logger.LogConfig()
 
+	// Load the configuration.
 	cfg, err := config.Load(*configFile)
 	if err != nil {
 		logrus.Panic(err)
 	}
 
+	// Initialise the Grafana API client.
 	client := grafana.NewClient(cfg.Grafana.BaseURL, cfg.Grafana.APIKey)
+	// Run the puller.
 	if err := PullGrafanaAndCommit(client, cfg); err != nil {
 		logrus.Panic(err)
 	}
--- src/pusher/main.go
+++ src/pusher/main.go
@@ -19,18 +19,25 @@ var (
 func main() {
 	var err error
 
+	// Define this flag in the main function because else it would cause a
+	// conflict with the one in the puller.
 	configFile := flag.String("config", "config.yaml", "Path to the configuration file")
 	flag.Parse()
 
+	// Load the logger's configuration.
 	logger.LogConfig()
 
+	// Load the configuration.
 	cfg, err := config.Load(*configFile)
 	if err != nil {
 		logrus.Panic(err)
 	}
 
+	// Initialise the Grafana API client.
 	grafanaClient := grafana.NewClient(cfg.Grafana.BaseURL, cfg.Grafana.APIKey)
 
+	// Set up either a webhook or a poller depending on the mode specified in the
+	// configuration file.
 	switch cfg.Pusher.Mode {
 	case "webhook":
 		err = webhook.Setup(cfg, grafanaClient, *deleteRemoved)
